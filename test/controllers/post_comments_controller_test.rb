d5a2a02cc6c8bd03829a3ccdf968b8b27d4c4c67
Jueri125 <o.jueri@gmail.com>
2024-04-30 09:01:06 +0000

    First commit.

---
+++ test/controllers/post_comments_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class PostCommentsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get post_comments_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get post_comments_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get post_comments_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get post_comments_edit_url
+    assert_response :success
+  end
+end
